3522b4ea4e27f5100854580cadedab2b4215f6de
Jonathan Rockway <jon@jrock.us>
2010-04-26 02:02:10 -0500

    add Stylish server

---
+++ lib/Stylish/Server.pm
@@ -0,0 +1,56 @@
+use MooseX::Declare;
+
+class Stylish::Server with (MooseX::LogDispatch, MooseX::Runnable) {
+    use AnyEvent::Socket;
+    use EV;
+    use Coro;
+    use Coro::EV;
+    use Coro::Debug;
+    use Coro::Handle;
+    use Stylish::Server::Session;
+    use Set::Object;
+
+    our $VERSION = '0.00_01';
+
+    has 'server' => (
+        is         => 'ro',
+        lazy_build => 1,
+    );
+
+    has 'sessions' => (
+        is      => 'ro',
+        isa     => 'Set::Object',
+        default => sub { Set::Object->new },
+        handles => {
+            register_session   => 'insert',
+            unregister_session => 'delete',
+        },
+    );
+
+    method _build_server {
+        return tcp_server 'unix/', '/tmp/stylish', sub { $self->run_session($_[0]) };
+    }
+
+    method run_session(GlobRef $fh){
+        return async {
+            my $session = Stylish::Server::Session->new(
+                fh     => unblock $fh,
+                server => $self,
+            );
+            $self->logger->info("New connection ". $session->id);
+            $Coro::current->desc("Stylish session ". $session->id);
+            $self->register_session($session);
+            $session->run;
+            $self->unregister_session($session);
+        };
+    }
+
+    method run {
+        my $loop = async { EV::loop };
+        my $debug = Coro::Debug->new_unix_server("/tmp/stylish-debug");
+        $loop->prio(3);
+        $self->server;
+        $self->logger->debug("Server listening on /tmp/stylish");
+        schedule;
+    }
+}
---
+++ lib/Stylish/Server/Session.pm
@@ -0,0 +1,60 @@
+use MooseX::Declare;
+
+class Stylish::Server::Session {
+    use JSON::XS;
+    use Try::Tiny;
+    use Scalar::Util qw(refaddr);
+
+    has 'id' => (
+        is      => 'ro',
+        isa     => 'Str',
+        lazy    => 1,
+        default => sub { q{}. refaddr($_[0]) },
+    );
+
+    has 'fh' => (
+        is       => 'ro',
+        isa      => 'Coro::Handle',
+        required => 1,
+        handles  => [qw/print readline/],
+    );
+
+    has 'server' => (
+        is       => 'ro',
+        isa      => 'Stylish::Server',
+        weak_ref => 1,
+        required => 1,
+    );
+
+    method run {
+        $self->print(encode_json({
+            welcome => {
+                session_id => $self->id,
+                version    => $self->server->VERSION,
+            },
+        }). "\n");
+
+        while(my $line = $self->readline("\n")){
+            my $cookie = undef;
+            try {
+                chomp $line;
+                my $req = decode_json($line);
+                my $cmd = delete $req->{command} || die 'no command?';
+                $cookie = delete $req->{cookie} || die 'no cookie?';
+                my $result = $self->run_command($cmd, $req);
+                $self->print(encode_json({
+                    command => $cmd,
+                    cookie  => $cookie,
+                    result  => $result,
+                }). "\n");
+            } catch {
+                $self->print(encode_json({ error => $_, cookie => $cookie }). "\n");
+            };
+        }
+    }
+
+    method run_command(Str $cmd, HashRef $args){
+        return { it => 'worked' };
+    }
+
+}
