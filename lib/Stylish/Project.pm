f9dc69617a19ef43d11eb5a7dc715ed939510337
Jonathan Rockway <jon@jrock.us>
2010-04-25 22:50:12 -0500

    fucking gitignore.

---
+++ lib/Stylish/Project.pm
@@ -0,0 +1,136 @@
+use MooseX::Declare;
+
+class Stylish::Project with AnyEvent::Inotify::EventReceiver {
+    use AnyEvent::Inotify::Simple;
+    use MooseX::FileAttribute;
+    use MooseX::MultiMethods;
+    use Devel::InPackage;
+    use Scalar::Util qw(weaken);
+    use File::Next::Filtered qw(files);
+    use MooseX::Types::Path::Class qw(File Dir);
+
+    has_directory 'root' => (
+        must_exist => 1,
+    );
+
+    has 'on_change' => (
+        is       => 'ro',
+        traits   => ['Array'],
+        isa      => 'ArrayRef[CodeRef]',
+        required => 1,
+        handles  => {
+            on_change_hooks => 'elements',
+            add_change_hook => 'push',
+        },
+    );
+
+    method run_on_change(@args) {
+        $_->(@args) for $self->on_change_hooks;
+    }
+
+    has 'inotify' => (
+        reader     => '_inotify',
+        isa        => 'AnyEvent::Inotify::Simple',
+        lazy_build => 1,
+    );
+
+    method _build_inotify {
+        my $i = AnyEvent::Inotify::Simple->new(
+            directory      => $self->root,
+            event_receiver => $self,
+        );
+
+        weaken $i->{event_receiver};
+
+        return $i;
+    }
+
+    has 'libraries' => (
+        is         => 'ro',
+        traits     => ['Hash'],
+        isa        => 'HashRef[ArrayRef[Str]]',
+        lazy_build => 1,
+        handles    => {
+            _get_libraries => 'keys',
+            _get_modules   => 'values',
+            _add_library   => 'set',
+            delete_library => 'delete',
+        },
+    );
+
+
+    multi method _is_library(Dir $file){ return }
+
+    multi method _is_library(File $file) {
+        return unless $file =~ /[.]pm$/;
+        return unless $file =~ /lib\W/;
+        return 1;
+    }
+
+    around _add_library(File|Dir $lib, ArrayRef $val){
+        return unless $self->_is_library($lib);
+        $self->$orig($lib, $val);
+    }
+
+    method extract_library_modules(File $file) {
+        my %packages;
+        Devel::InPackage::scan(
+            file     => $file->absolute($self->root)->stringify,
+            callback => sub { $packages{$_[1]}++; 1 },
+        );
+        return [ grep { $_ ne 'main' } keys %packages ];
+    }
+
+    method add_library(File $lib){
+        $self->_add_library($lib, $self->extract_library_modules($lib));
+    }
+
+    method get_libraries {
+        map { Path::Class::file($_) } $self->_get_libraries;
+    }
+
+    method get_modules {
+        map { @{ $_ || [] } } $self->_get_modules;
+    }
+
+    method _build_libraries {
+        # normally, Inotify::Simple filters everything, but the first
+        # time around, we do it ourselves
+        my $i = files(
+            { filters => [qw/Backup VersionControl EditorJunk/] },
+            $self->root,
+        );
+
+        my @result;
+        while(my $file = $i->()){
+            $file = $file->relative($self->root);
+            push @result, $file if $self->_is_library($file);
+        }
+        return { map { $_ => $self->extract_library_modules($_) } @result };
+    }
+
+    method handle_access {}
+    method handle_attribute_change {}
+    method handle_close {}
+    method handle_open {}
+
+    method handle_create(File $file) {
+        $self->add_library($file);
+        $self->run_on_change($file);
+    }
+
+    method handle_move(File $file) {
+        $self->run_on_change($file);
+    }
+
+    method handle_modify(File $file) {
+        $self->run_on_change($file);
+    }
+
+   method handle_delete(File $file) {
+        $self->delete_library($file);
+        $self->run_on_change($file);
+    }
+
+    method BUILD { $self->_inotify }
+}
---
+++ lib/Stylish/REPL/Project.pm
@@ -0,0 +1,125 @@
+use MooseX::Declare;
+
+class Stylish::REPL::Project {
+    use Stylish::Project;
+    use AnyEvent::REPL;
+    use Coro;
+    use Coro::Semaphore;
+
+    has 'project' => (
+        is       => 'ro',
+        isa      => 'Stylish::Project',
+        required => 1,
+        trigger  => sub {
+            my ($self, $project) = @_;
+            $project->add_change_hook(sub { $self->change });
+        }
+    );
+
+    has 'repl_version' => (
+        reader   => 'get_repl_version',
+        init_arg => undef,
+        default  => 0,
+        traits   => ['Counter'],
+        handles  => { 'inc_repl_version' => 'inc' },
+    );
+
+    has 'good_repl' => (
+        is         => 'rw',
+        isa        => 'AnyEvent::REPL',
+        lazy_build => 1,
+    );
+
+    has 'on_output' => (
+        is       => 'ro',
+        isa      => 'CodeRef',
+        default  => sub { sub {} },
+    );
+
+    has 'on_repl_change' => (
+        is       => 'ro',
+        isa      => 'CodeRef',
+        default  => sub { sub {} },
+    );
+
+    has 'reloading' => (
+        accessor => 'reloading',
+        isa      => 'Coro::Semaphore',
+        default  => sub { Coro::Semaphore->new(1) },
+    );
+
+    method _build_good_repl {
+        my $r = AnyEvent::REPL->new;
+        async { $self->_setup_repl_pwd($r) }->join;
+        return $r;
+    }
+
+    method repl_eval(AnyEvent::REPL $repl, Str $code) {
+        my $cb = Coro::rouse_cb;
+        #warn "eval $code";
+        $repl->push_eval(
+            $code,
+            on_output => $self->on_output,
+            on_result => sub { $cb->( result => @_ ) },
+            on_error  => sub { $cb->( error  => @_ ) },
+        );
+        my @result = Coro::rouse_wait;
+        my $status = shift @result;
+        confess "REPL error: @result" if $status eq 'error';
+        return @result if wantarray;
+        return join '', @result;
+    }
+
+    method _setup_repl_pwd(AnyEvent::REPL $repl){
+        my $dir = $self->project->root->resolve->absolute;
+        my $lib = $dir->subdir('lib');
+        $self->repl_eval($repl, qq{chdir "\Q$dir\E";});
+        $self->repl_eval($repl, qq{use lib "\Q$lib\E"});
+    }
+
+    method _load_modules_in_repl(AnyEvent::REPL $repl, Bool $strict_load? = 1){
+        my @modules = $self->project->get_libraries;
+        my $error = 'unknown error';
+        my $result = eval {
+            $self->_setup_repl_pwd($repl);
+            if ($strict_load) {
+                $self->repl_eval($repl, qq{require "\Q$_\E"}) for @modules;
+            }
+            else {
+                $self->repl_eval($repl, qq{eval { require "\Q$_\E" }}) for @modules;
+            }
+            return $self->repl_eval($repl, qq{2 + 2});
+        };
+        $error = $@ if $@;
+        return $repl if defined $result && $result eq '4';
+        confess "Modules failed to load in the new REPL: $error";
+    }
+
+    method new_repl {
+        my $r = AnyEvent::REPL->new;
+        $self->_load_modules_in_repl($r);
+        return $r;
+    }
+
+    method change {
+        async {
+            $self->reloading->down;
+            my $new_repl = eval { $self->new_repl };
+            $self->on_output($@) if $@;
+            if($new_repl){
+                # get lexenv from old repl
+                $self->good_repl($new_repl);
+                $self->inc_repl_version;
+                $self->on_repl_change->($self->get_repl_version);
+            }
+            $self->reloading->up;
+        };
+    }
+
+    # not really; we just block until it's done
+    method push_eval(Str $code, Bool $chomp? = 1){
+        my $result = $self->repl_eval($self->good_repl, $code);
+        chomp $result if $chomp;
+        return $result;
+    }
+}
